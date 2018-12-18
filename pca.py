bdbde309d75361b841341a769864b47f79cd7561
Alex Chang <zfmedisonxfmt@126.com>
2018-12-18 08:33:09 +0800

    pca

---
+++ pca.py
@@ -0,0 +1,59 @@
+import utils
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.decomposition import PCA, FactorAnalysis
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+
+(trainInput, trainTarget) = utils.loadTrainData()
+#(testImgId, validTestData) = utils.loadTestData()
+
+n_components = np.arange(5, 40, 5)  # options for n_components
+
+def compute_scores(X):
+    pca = PCA(svd_solver='full')
+    fa = FactorAnalysis()
+
+    pca_scores, fa_scores = [], []
+    for n in n_components:
+        print("N: {}".format(n))
+        pca.n_components = n
+        fa.n_components = n
+        pca_scores.append(np.mean(cross_val_score(pca, X, cv=5)))
+        fa_scores.append(np.mean(cross_val_score(fa, X, cv=5)))
+
+    return pca_scores, fa_scores
+
+X = trainInput
+title = 'Noise'
+pca_scores, fa_scores = compute_scores(X)
+n_components_pca = n_components[np.argmax(pca_scores)]
+n_components_fa = n_components[np.argmax(fa_scores)]
+
+pca = PCA(svd_solver='full', n_components='mle')
+pca.fit(X)
+n_components_pca_mle = pca.n_components_
+
+print("best n_components by PCA CV = %d" % n_components_pca)
+print("best n_components by FactorAnalysis CV = %d" % n_components_fa)
+print("best n_components by PCA MLE = %d" % n_components_pca_mle)
+
+plt.figure()
+plt.plot(n_components, pca_scores, 'b', label='PCA scores')
+plt.plot(n_components, fa_scores, 'r', label='FA scores')
+plt.axvline(n_components_pca, color='b',
+            label='PCA CV: %d' % n_components_pca, linestyle='--')
+plt.axvline(n_components_fa, color='r',
+            label='FactorAnalysis CV: %d' % n_components_fa,
+            linestyle='--')
+plt.axvline(n_components_pca_mle, color='k',
+            label='PCA MLE: %d' % n_components_pca_mle, linestyle='--')
+
+plt.xlabel('nb of components')
+plt.ylabel('CV scores')
+plt.legend(loc='lower right')
+plt.title(title)
+
+plt.show()
