b4ebe45331e8c820b40b842998e419df5e25082e
Alex Chang <alexzhangfm@126.com>
2018-12-20 09:53:32 +0800

    logReg gs4

--- model/logReg.py
+++ model/logReg.py
@@ -19,10 +19,10 @@ param_grid = [{'solver': ['newton-cg', 'lbfgs', 'sag', 'saga'], 'penalty': ['l2'
                'max_iter': [10000]}]
 '''
 
-param_grid = [{'solver': ['liblinear'], 'penalty': ['l1', 'l2'],
-               'dual': [False], 'C': [1, 10, 100, 1000], 'tol': [1e-2, 1e-3, 1e-4, 1e-5],
+param_grid = [{'solver': ['liblinear'], 'penalty': ['l2'],
+               'dual': [True], 'C': [1, 10, 100, 1000], 'tol': [1e-2, 1e-3, 1e-4, 1e-5],
                'class_weight': [None, 'balanced'], 'multi_class': ['ovr'],
-               'max_iter': [10000]},]
+               'max_iter': [10000]}]
 
 def getModel(hyperParameter=None):
     if hyperParameter == None:
